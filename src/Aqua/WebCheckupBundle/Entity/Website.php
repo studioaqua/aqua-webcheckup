7db64cf47b0a4b27642062ec329f24ff6c1cf32f
Roberto Peruzzo <roberto.peruzzo@gmail.com>
2015-11-29 14:56:21 +0100

    Added versioning timestamp on website entity.

--- src/Aqua/WebCheckupBundle/Entity/Website.php
+++ src/Aqua/WebCheckupBundle/Entity/Website.php
@@ -16,77 +16,105 @@ use Doctrine\ORM\Mapping as ORM;
 class Website
 {
 
-  /**
-   * @ORM\Column(type="integer", name="wid")
-   * @ORM\Id
-   * @ORM\GeneratedValue(strategy="AUTO")
-   */
-  protected $websiteId;
+    /**
+    * @ORM\Column(type="integer", name="wid")
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="AUTO")
+    */
+    protected $websiteId;
 
-  /**
-   * @ORM\Column(length=255)
-   * @Assert\Url()
-   */
-  protected $website;
+    /**
+    * @ORM\Column(length=255)
+    * @Assert\Url()
+    */
+    protected $website;
 
-  /**
-   * @ORM\Column(type="boolean", name="mobile_ready")
-   */
+    /**
+    * @ORM\Column(type="boolean", name="mobile_ready")
+    */
 
-  protected $mobileReady = FALSE;
+    protected $mobileReady = FALSE;
 
-  /**
-   * @ORM\Column(type="integer", name="mobile_score")
-   */
-  protected $mobileScore = 0;
+    /**
+    * @ORM\Column(type="integer", name="mobile_score")
+    */
+    protected $mobileScore = 0;
 
-  /**
-   * @ORM\Column(type="datetime")
-   */
-  protected $created;
+    /**
+    * @ORM\Column(name="created", type="datetime")
+    * @ORM\Version
+    */
+    protected $created;
 
 
-  public function getWebsite()
-  {
+    public function getWebsite()
+    {
       return $this->website;
-  }
+    }
 
-  public function setWebsite($website)
-  {
+    public function setWebsite($website)
+    {
       $this->website = $website;
-  }
+    }
 
-  public function getName()
-  {
-      return $this->name;
-  }
-
-  public function setName($name)
-  {
-      $this->name = $name;
-  }
-
-  public function isMobileReady()
-  {
+    /**
+    * Get mobileReady
+    *
+    * @return boolean
+    */
+    public function getMobileReady()
+    {
       return $this->mobileReady;
-  }
-  public function isMobileReadyLiteral()
-  {
+    }
+    public function isMobileReadyLiteral()
+    {
       return ($this->mobileReady) ? 'Yes' : 'No';
-  }
-
-  public function setMobileReady($mobileReady)
-  {
+    }
+    public function setMobileReady($mobileReady)
+    {
       $this->mobileReady = $mobileReady;
-  }
+    }
 
-  public function getMobileScore()
-  {
+    public function getMobileScore()
+    {
       return $this->mobileScore;
-  }
+    }
 
-  public function setMobileScore($mobileScore)
-  {
+    public function setMobileScore($mobileScore)
+    {
       $this->mobileScore += $mobileScore;
-  }
+    }
+
+    /**
+     * Get websiteId
+     *
+     * @return integer
+     */
+    public function getWebsiteId()
+    {
+        return $this->websiteId;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Website
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
 }
