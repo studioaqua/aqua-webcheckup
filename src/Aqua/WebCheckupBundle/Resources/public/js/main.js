4ec223649fbe6e6906d273d6a644fe5e5d8d34fd
Roberto Peruzzo <roberto.peruzzo@gmail.com>
2015-12-18 17:21:59 +0100

    Fixed nprogress visualization in safari.

--- src/Aqua/WebCheckupBundle/Resources/public/js/main.js
+++ src/Aqua/WebCheckupBundle/Resources/public/js/main.js
@@ -1,6 +1,9 @@
-+function ($) {
+jQuery.noConflict();
+
+(function( $ ) {
   $( "#form_webcheckup" ).submit(function( event ) {
+    console.log(event);
     $('#form_checkup').prop('disabled', true);
     NProgress.start();
   });
-}(jQuery);
+})( jQuery );
